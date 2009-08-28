179670ad423a39d9ad1371a36f8feb567438c208
Mathieu MARACHE <mathieu.marache@cstb.fr>
2009-08-28 17:58:01 +0200

    p173 added login adn remember where we wedre

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -2,6 +2,9 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  before_filter :authorize, :except => :login
+  session :session_key => 'tasktracker_session_id'
+  
   helper :all # include all helpers, all the time
 
   # See ActionController::RequestForgeryProtection for details
@@ -11,5 +14,16 @@ class ApplicationController < ActionController::Base
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
-  # filter_parameter_logging :password
+  filter_parameter_logging :password
+  
+  
+protected
+  def authorize
+    unless User.find_by_id(session[:user_id])
+      session[:original_uri] = request.request_uri
+      flash[:notice] = "Please log in"
+      redirect_to :controller => :auth, :action => :login
+    end
+  end
 end
+
